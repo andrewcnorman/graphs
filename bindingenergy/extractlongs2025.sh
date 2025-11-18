658aa26c855203a183485be90af42d9869e342cd
Andrew Norman <andrewcnorman@gmail.com>
2025-11-18 15:36:53 +0000

    Updating all data / notebook for 2020 data release

---
+++ bindingenergy/extractlongs2025.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Andrew C. Norman
+# Radley College
+# 2025-11-18
+# 
+# Extracts the data for the stable elements only to longsdata.dat
+# Usage: ./extractlongs2025.sh
+
+echo "# stable nuclide data (a subset of 'data2020.dat')" > longsdata2020.dat
+
+cat longs.dat | while read LINE
+do
+# put A and Z for that nuclide into variables
+A=$(echo "$LINE" | cut --delimiter=" " -f 1)
+Z=$(echo "$LINE" | cut --delimiter=" " -f 2)
+# now find the data for stable nuclide and put it into data.dat
+grep -E "^$A $Z" data2020.dat >> longsdata2020.dat
+done
